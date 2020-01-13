995a24d4c47e51c83ec7a481d32c5e8be515051e
juliofti <43100202+juliofti@users.noreply.github.com>
2020-01-12 21:15:38 -0300

    Inicialização do projeto

---
+++ src/main/java/com/julio/helpdesk/SpringApiRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.julio.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestFullApplication.class, args);
+	}
+
+}
