2d2d194f994713b3567b8b3f33778a50406c33d0
juliofti <43100202+juliofti@users.noreply.github.com>
2020-01-18 09:31:18 -0300

    Criado classe JwtUserFactory

---
+++ src/main/java/com/julio/helpdesk/api/security/jwt/JwtUserFactory.java
@@ -0,0 +1,27 @@
+package com.julio.helpdesk.api.security.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.julio.helpdesk.api.entity.User;
+import com.julio.helpdesk.api.entity.enums.ProfileEnum;
+
+public class JwtUserFactory {
+
+	
+	private JwtUserFactory() {
+	}
+	
+	public static JwtUser create(User user) {
+		return new JwtUser(user.getId(), user.getEmail(), user.getPassword(), mapToGranteAuthorities(user.getProfile()));
+	}
+	
+	private static List<GrantedAuthority> mapToGranteAuthorities(ProfileEnum profileEnum) {
+		List<GrantedAuthority> authorities  = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority(profileEnum.toString()));
+		return authorities;
+	}
+}
