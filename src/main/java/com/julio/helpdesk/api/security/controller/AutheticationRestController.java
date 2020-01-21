587c8d4c1b9a22a5ea2e69fb1a022f096ad99590
julio xavier <julio.a.x.10@gmail.com>
2020-01-21 13:27:03 -0300

    Criado classe controller da autenticação.

---
+++ src/main/java/com/julio/helpdesk/api/security/controller/AutheticationRestController.java
@@ -0,0 +1,68 @@
+package com.julio.helpdesk.api.security.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.julio.helpdesk.api.entity.User;
+import com.julio.helpdesk.api.security.jwt.JwtAuthenticationRequest;
+import com.julio.helpdesk.api.security.jwt.JwtTokenUtil;
+import com.julio.helpdesk.api.security.model.CurrentUser;
+import com.julio.helpdesk.api.service.UserService;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AutheticationRestController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
+	@Autowired
+	private UserDetailsService userdetailsService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping(value="/api/auth")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest){
+		
+		final Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		final UserDetails userDetails = userdetailsService.loadUserByUsername(authenticationRequest.getEmail());
+		final String token = jwtTokenUtil.generateToken(userDetails);
+		final User user = userService.findByEmail(authenticationRequest.getEmail());
+		user.setPassword(null);
+		return ResponseEntity.ok(new CurrentUser(token, user));
+	}
+	
+	@PostMapping(value="/api/refresh")
+	public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request){
+		String token = request.getHeader("Authorization");
+		String username = jwtTokenUtil.getUsernameFromToken(token);
+		final User user = userService.findByEmail(username);
+		
+		if(jwtTokenUtil.canTokenBeRefreshed(token)) {
+			String refreshedToken = jwtTokenUtil.refreshToken(token);
+			return ResponseEntity.ok(new CurrentUser(refreshedToken, user));
+		}else {
+			return ResponseEntity.badRequest().body(null);
+		}
+	}
+}
