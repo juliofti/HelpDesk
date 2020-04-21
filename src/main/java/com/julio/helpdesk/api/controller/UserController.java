109527fe9166f8c506e9815b796a2d423e5c7f54
juliofti <43100202+juliofti@users.noreply.github.com>
2020-04-20 21:03:05 -0300

    Create class UserController.java with method create

---
+++ src/main/java/com/julio/helpdesk/api/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.julio.helpdesk.api.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.julio.helpdesk.api.entity.User;
+import com.julio.helpdesk.api.response.Response;
+import com.julio.helpdesk.api.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "*")//autoriza todas portas do servidor
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private PasswordEncoder passwordEnconder;
+	
+	@PostMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> create(HttpServletRequest request, @RequestBody User user, BindingResult result){
+		
+		Response<User> response = new Response<User>();
+		try {
+			validateCreateUser(user, result);
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			user.setPassword(passwordEnconder.encode(user.getPassword()));
+			User userPersisted = userService.createOrUpdate(user);
+			response.setData(userPersisted);
+		}catch(DuplicateKeyException dE) {
+			response.getErrors().add("E-mais already registered !");
+			return ResponseEntity.badRequest().body(response);
+		}catch(Exception ex) {
+			response.getErrors().add(ex.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		return ResponseEntity.ok(response);
+	}
+	
+	private void validateCreateUser(User user, BindingResult result) {
+		if(user.getEmail() == null) {
+			result.addError(new ObjectError("User", "Email no information"));
+		}
+	}
+	
+}
